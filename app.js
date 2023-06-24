11c7e5799debb18d247fefdf9e368be5483bd1c3
ShrutiSharma77 <shruti.sharma.met21@itbhu.ac.in>
2023-06-24 21:13:54 +0530

    adeed nift buttons

--- app.js
+++ app.js
@@ -13,47 +13,8 @@ app.use(cors());
 
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/news', async (req, res) => {
-  try {
-    const apiUrl = `https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=AAPL&apikey=9R07HDWXSPTYIOF6`;
 
-    const response = await axios.get(apiUrl);
-    const feed = response.data.feed;
-    res.status(200).send(feed);
-    
-  } catch (error) {
-    console.error('Error occurred:', error.message);
-    res.status(500).send('Error occurred');
-  }
-});
-
-async function updateNews() {
-    try {
-      const apiUrl = `https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=AAPL&apikey=9R07HDWXSPTYIOF6`;
-  
-      const response = await axios.get(apiUrl);
-      const feed = response.data.feed;
-  
-      // Update the news data in your database or cache
-      // ...
-  
-      console.log('News updated successfully.');
-    } catch (error) {
-      console.error('Error occurred:', error.message);
-    }
-}
-  
-// Schedule the updateNews function to run daily at 12:00 AM
-cron.schedule('0 3 * * *', updateNews);
-
-
-
-
-
-
-
-
-const port = 5001;
+const port = 5002;
 
 app.use(express.static('public'));
 
